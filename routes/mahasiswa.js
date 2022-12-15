db463c9a48f6d90cf1f4c68bd3e04bd29d628372
brodytiktok <105333102+brodytiktok@users.noreply.github.com>
2022-12-15 17:06:11 +0700

    Upload

--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -23,11 +23,49 @@ router.post('/', async (req, res) => {
     }
 })
 
-// Get
 // Read
 router.get('/', async (req, res) => {
     try {
         const mahasiswa = await Mahasiswa.find()
+        res.json(mahasiswa)
+    } catch (error) {
+        res.json({
+            message: error
+        })
+    }
+})
+
+
+// Update 
+router.put('/:mahasiswaId', async (req, res) => {
+    // tampung input mahasiswa 
+    const data = {
+        nama: req.body.nama,
+        alamat: req.body.alamat
+    }
+
+    try {
+        // update data 
+        const mahasiswa = await Mahasiswa.updateOne({
+            _id: req.params.mahasiswaId
+        }, data)
+        // response
+        res.json(mahasiswa)
+    } catch (error) {
+        res.json({
+            message: error
+        })
+    }
+})
+
+// Delete 
+router.delete('/:mahasiswaId', async (req, res) => {
+    try {
+        // delete data 
+        const mahasiswa = await Mahasiswa.deleteOne({
+            _id: req.params.mahasiswaId
+        })
+        // response
         res.json(mahasiswa)
     } catch (error) {
         res.json({
